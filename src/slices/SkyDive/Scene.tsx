893d0003d832c570b7733667506dbcd4afc83013
Akaikenlol <keithken972@gmail.com>
2024-10-20 21:47:09 +0630

    feat: Finished Implementing Diving Can Animation

--- src/slices/SkyDive/Scene.tsx
+++ src/slices/SkyDive/Scene.tsx
@@ -59,6 +59,72 @@ const Scene = ({ sentence, flavor }: Props) => {
       repeat: -1,
       ease: "none",
     });
+
+    // Infinite cloud movement
+    const DISTANCE = 15;
+    const DURATION = 6;
+
+    gsap.set([cloud2Ref.current?.position, cloud1Ref.current?.position], {
+      ...getXYPositions(DISTANCE),
+    });
+
+    gsap.to(cloud1Ref.current?.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * 2)}`,
+      ease: "none",
+      repeat: -1,
+      duration: DURATION,
+    });
+
+    gsap.to(cloud2Ref.current?.position, {
+      y: `+=${getYPosition(DISTANCE * 2)}`,
+      x: `+=${getXPosition(DISTANCE * 2)}`,
+      ease: "none",
+      repeat: -1,
+      delay: DURATION / 2,
+      duration: DURATION,
+    });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".skydive",
+        pin: true,
+        start: "top top",
+        end: "+=2000",
+        scrub: 1.5,
+      },
+    });
+
+    scrollTl
+      .to("body", {
+        backgroundColor: "#C0F0F5",
+        overwrite: "auto",
+        duration: 0.1,
+      })
+      .to(cloudsRef.current.position, { z: 0, duration: 0.3 }, 0)
+      .to(canRef.current.position, {
+        x: 0,
+        y: 0,
+        duration: 0.3,
+        ease: "back.out(1.7)",
+      })
+      .to(
+        wordsRef.current.children.map((word) => word.position),
+        {
+          keyframes: [
+            { x: 0, y: 0, z: -1 },
+            { ...getXYPositions(-7), z: -7 },
+          ],
+          stagger: 0.3,
+        },
+        0,
+      )
+      .to(canRef.current.position, {
+        ...getXYPositions(4),
+        duration: 0.5,
+        ease: "back.in(1.7)",
+      })
+      .to(cloudsRef.current.position, { z: 7, duration: 0.5 });
   });
 
   return (
@@ -70,9 +136,10 @@ const Scene = ({ sentence, flavor }: Props) => {
           rotationIntensity={0}
           floatIntensity={3}
           floatSpeed={3}
-        ></FloatingCan>
+        >
+          <pointLight intensity={30} color={"#8C0413"} decay={0.6} />
+        </FloatingCan>
       </group>
-      <FloatingCan ref={canRef}></FloatingCan>
 
       <Cloud ref={cloudsRef}>
         <Cloud ref={cloud1Ref} bounds={[10, 10, 2]} />
